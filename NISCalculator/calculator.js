de46fe63238d28c47134caddd55d886ca1eedcc8
PengChen/陈鹏 <32901775+PengChen2016@users.noreply.github.com>
2020-10-26 12:26:56 +0800

    Add files via upload
    内容与显示更新

--- NISCalculator/calculator.js
+++ NISCalculator/calculator.js
@@ -11,34 +11,35 @@ function p_compute(form){
 （3）document.forms['exportServlet']：表示获取当前页面的name="exportServlet"的表单  */
     var ne = document.forms['p-input'].ne.value;
     var Te = document.forms['p-input'].Te.value;
-    var Ti = document.forms['p-input'].Tpi.value;
-    var A = document.forms['p-input'].Api.value;
-    var Z = document.forms['p-input'].Zpi.value;
-    var B = document.forms['p-input'].B.value;
-	// 3位小数的科学计数法
-    var en = 3;
+    var Ti = document.forms['p-input'].Ti.value;
+    var A = document.forms['p-input'].Ai.value;
+    var Z = document.forms['p-input'].Zi.value;
+    var B = 1.0e-3*document.forms['p-input'].B.value;
+    var en = 2; // 小数位数
 	var omega_pe;
 	var v_te;
+	var v_ti;
 	var omega_ce;
 	var omega_ci;
 	const epsilon0=8.854e-12;
 	const echarge=1.6e-19;
 	const me=9.109e-31;
-	const mproton=1.67e-27
+	const mproton=1.67e-27;
+    const pi=3.1415926;
 	
 
     // Frequencies
 	omega_pe=5.641*10*Math.sqrt(ne);
     document.forms['p-output-frequency'].omega_pe.value = (omega_pe).toExponential(en);
-    document.forms['p-output-frequency'].inverse_omega_pe.value = (1/omega_pe).toExponential(en);
     omega_ce=1.759e11*B;
 	document.forms['p-output-frequency'].omega_ce.value = (omega_ce).toExponential(en);
     omega_ci=9.577e7*B*Z/A
 	document.forms['p-output-frequency'].omega_ci.value = (omega_ci).toExponential(en);
-	document.forms['p-output-frequency'].f_pe.value = (omega_pe/2/3.1415926).toExponential(en);
-	document.forms['p-output-frequency'].f_ce.value = (omega_ce/2/3.1415926).toExponential(en);
-	document.forms['p-output-frequency'].f_ci.value = (omega_ci/2/3.1415926).toExponential(en);
 	
+    // Time
+    document.forms['p-output-time'].inverse_omega_pe.value = (1/omega_pe).toExponential(en);
+    document.forms['p-output-time'].T_ce.value = (2*pi/omega_ce).toExponential(en);
+	document.forms['p-output-time'].T_ci.value = (2*pi/omega_ci).toExponential(en);
 
 	// Velocities
 	v_te=4.194e5*Math.sqrt(Te);
@@ -48,13 +49,13 @@ function p_compute(form){
     document.forms['p-output-velocity'].v_meane.value = (1.732*v_te).toExponential(en);
     document.forms['p-output-velocity'].v_bohm.value = (Math.sqrt(echarge*Te/A/mproton)).toExponential(en);
 	v_ti=9.786e3*Math.sqrt(Ti/A);
-    document.forms['p-output-velocity'].v_i.value = (1.414*v_ti).toExponential(en);
+    document.forms['p-output-velocity'].v_ti.value = (v_ti).toExponential(en);
 	
 	
     // Lengths
     document.forms['p-output-length'].lambda_d.value = (7.439e3*Math.sqrt(Te/ne)).toExponential(en);
-    document.forms['p-output-length'].r_ce.value = (v_te/omega_ce).toExponential(en);
-    document.forms['p-output-length'].r_ci.value = (v_ti/omega_ci).toExponential(en);
+    document.forms['p-output-length'].r_ce.value = (1.0e3*(v_te/omega_ce)).toExponential(en);
+    document.forms['p-output-length'].r_ci.value = (1.0e3* (v_ti/omega_ci)).toExponential(en);
 
     return;
 } 
